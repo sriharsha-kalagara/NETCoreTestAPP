cb8218b371c09a94af1a930ba4206e4fd76bc15d
Kalagara, Sri <kalagarash@upmc.edu>
2020-05-13 20:39:40 -0400

    hello property

--- NETCoreTestAPP/WeatherForecast.cs
+++ NETCoreTestAPP/WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace NETCoreTestAPP
     {
         public DateTime Date { get; set; }
 
+        public string Hello { get; set; }
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
